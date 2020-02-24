6b1927c6192a739b741970523016d6088b8a3062
clarkmyfancy <clark.jack0@gmail.com>
2020-02-24 03:35:41 -0600

    requests to server are srtings and thanks to pickle can get objects as responses

--- network.py
+++ network.py
@@ -15,16 +15,17 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.address)
-            return pickle.loads(self.client.recv(2048))
+            return self.client.recv(2048).decode() # instead of pickling here becasue want to assign the object a zero or a oen by the computer and that is the easiest way, allows for array stuff
         except:
             print("Connection not successful")
 
     def get_player(self):
         return self.player
 
-    def send(self, player):
+    def send(self, data):
         try:
-            self.client.send(pickle.dumps(player))
+            # want to send lightweight things and get back objects
+            self.client.send(str.encode(data))
             player = pickle.loads(self.client.recv(2048))
             return player
         except socket.error as e:
